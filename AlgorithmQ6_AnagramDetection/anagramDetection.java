55d8934d220141e36d2e0432fbb864ba20dfdadf
asita26 <asita98890@gmail.com>
2023-12-11 10:05:38 +0530

    Adding q6 from Algorithm

---
+++ AlgorithmQ6_AnagramDetection/anagramDetection.java
@@ -0,0 +1,44 @@
+package AlgorithmQ6_AnagramDetection;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class anagramDetection {
+
+    public static boolean areAnagrams(String str1, String str2) {
+        // Remove spaces and convert to lowercase for case-insensitive comparison
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // Check if the lengths are the same after removing spaces
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert strings to char arrays and sort them
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        // Compare the sorted char arrays
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    public static void main(String[] args) {
+        // Input: Take two strings as input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String str1 = scanner.nextLine();
+        System.out.print("Enter the second string: ");
+        String str2 = scanner.nextLine();
+
+        // Check for Anagrams
+        if (areAnagrams(str1, str2)) {
+            System.out.println("The two strings are anagrams.");
+        } else {
+            System.out.println("The two strings are not anagrams.");
+        }
+    }
+}
